ba3b645197643015603cbe894dee41d52e4446c6
harshavardhan <yharsha111@gmail.com>
2020-04-11 13:23:33 +0530

    Spring Security basic app

---
+++ spring-security-demo-01-baseapp/src/main/java/com/learn/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,27 @@
+package com.learn.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.learn.springsecurity.demo")
+public class DemoAppConfig {
+	
+	//Bean for View Resolver
+	@Bean
+	public ViewResolver viewResolver()
+	{
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+}
---
+++ spring-security-demo-01-baseapp/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+
+
+<head>
+	<title>Company Home Page</title>
+</head>
+
+
+<body>
+	<h2>Company Home Page</h2>
+	
+	<hr>
+	
+	Welcome to Company Home Page
+</body>
+</html>
