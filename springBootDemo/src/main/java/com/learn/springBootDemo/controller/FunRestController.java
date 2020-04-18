dfcc4eec239fd7a1400de7f1ff12e1e39611924e
harshavardhan <yharsha111@gmail.com>
2020-04-18 19:59:04 +0530

    SpringBootApp

---
+++ springBootDemo/src/main/java/com/learn/springBootDemo/controller/FunRestController.java
@@ -0,0 +1,17 @@
+package com.learn.springBootDemo.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "Hello World"+LocalDateTime.now();
+	}
+
+}
