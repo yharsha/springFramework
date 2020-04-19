5657fb27c37641f212362aa4651b99f6196adb6d
harshavardhan <yharsha111@gmail.com>
2020-04-19 11:22:02 +0530

    application properties

--- springBootDemo/src/main/java/com/learn/springBootDemo/controller/FunRestController.java
+++ springBootDemo/src/main/java/com/learn/springBootDemo/controller/FunRestController.java
@@ -2,16 +2,32 @@ package com.learn.springBootDemo.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class FunRestController {
+	
+	@Value("${coach.name}")
+	private String coachname;
+
+	@Value("${team.name}")
+	private String teamName;
 	
 	@GetMapping("/")
 	public String sayHello()
 	{
 		return "Hello World to you at timestamp: "+LocalDateTime.now();
+	}
+	
+	
+	@GetMapping("/teaminfo")
+	public String teaminfo()
+	{
+		return "Coach name: "+coachname +"Team Name: " + teamName +"  %% "+LocalDateTime.now();
 	}
 
 }
