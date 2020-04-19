a9dbbd3e99cef3013fcd6079fe401a0fcaeb144c
harshavardhan <yharsha111@gmail.com>
2020-04-19 10:26:36 +0530

    Dev Tools and actuators

--- springBootDemo/src/main/java/com/learn/springBootDemo/controller/FunRestController.java
+++ springBootDemo/src/main/java/com/learn/springBootDemo/controller/FunRestController.java
@@ -11,7 +11,7 @@ public class FunRestController {
 	@GetMapping("/")
 	public String sayHello()
 	{
-		return "Hello World"+LocalDateTime.now();
+		return "Hello World to you at timestamp: "+LocalDateTime.now();
 	}
 
 }
